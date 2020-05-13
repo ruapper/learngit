6f0ec0726b5fd73f4530076f3d9e6e7aaba0bc95
ruapper <237727588@qq.com>
2020-05-13 21:55:01 +0800

    Add files via upload

---
+++ burden5/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+        <router-view/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+}
+</script>
+
+<style>
+body,#app,html {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+</style>
